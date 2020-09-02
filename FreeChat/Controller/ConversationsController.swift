5a81b2559d07615e9c0eea895aded37b7dbf749c
ngodangtan1994@gmail.com <ngodangtan@MacBook-Pro-cua-Ngo.local>
2020-09-02 15:01:30 +0700

    create project

---
+++ FreeChat/Controller/ConversationsController.swift
@@ -0,0 +1,90 @@
+//
+//  ConversationsController.swift
+//  FreeChat
+//
+//  Created by Ngo Dang tan on 9/2/20.
+//  Copyright © 2020 Ngo Dang Tan. All rights reserved.
+//
+
+import UIKit
+private let reuseIdentifer = "ConversationCell"
+class ConversationsController: UIViewController{
+    // MARK: - Properties
+    
+    private let tableView = UITableView()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+     
+    }
+    // MARK: - Selectors
+    
+    @objc func showProfile(){
+        print(123)
+    }
+    
+    // MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .white
+
+        configureNavigationBar()
+        configureTableView()
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifer)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
+    
+    func configureNavigationBar(){
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+        
+    }
+    
+}
+extension ConversationsController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath)
+        cell.textLabel?.text = "Trang cho"
+        return cell
+    }
+    
+    
+}
+
+extension ConversationsController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
---
+++ FreeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeChat
+//
+//  Created by Ngo Dang tan on 9/2/20.
+//  Copyright © 2020 Ngo Dang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
